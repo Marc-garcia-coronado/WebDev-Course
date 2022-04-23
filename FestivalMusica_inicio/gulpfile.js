f15fbcf75e3b3f548d53427eb4effab50b8fbd2a
Marc Garcia <marcgaco@gmail.com>
2022-04-23 23:10:28 +0200

    23/04/2022

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch } = require("gulp");
-const gulpPlumber = require("gulp-plumber");
+const {src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const avif = require("gulp-avif");
+const cache = require("gulp-cache");
+const webp = require("gulp-webp");
+const imagemin = require("gulp-imagemin");
 
 function css(done) {
     
@@ -16,5 +23,43 @@ function dev(done) {
     watch("src/scss/**/*.scss", css);
     done();
 }
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"));
+    done();
+}
+
+function versionAvif(done) {
+
+    const opciones = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(avif(opciones))
+        .pipe(dest("build/img"));
+    done();
+}
+
+function imagenes(done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(versionWebp, versionAvif, imagenes, dev);
