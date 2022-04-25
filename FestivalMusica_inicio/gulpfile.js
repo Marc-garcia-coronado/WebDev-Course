93dc866fce79c98c1bdf4df0cb9737a3bb3ccc5e
Marc Garcia <marcgaco@gmail.com>
2022-04-25 19:31:48 +0200

    FestivalMusica finished

--- FestivalMusica_inicio/gulpfile.js
+++ FestivalMusica_inicio/gulpfile.js
@@ -3,6 +3,13 @@ const {src, dest, watch, parallel } = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
+// JS
+const terser = require("gulp-terser-js");
 
 // Imagenes
 const avif = require("gulp-avif");
@@ -13,16 +20,33 @@ const imagemin = require("gulp-imagemin");
 function css(done) {
     
     src("src/scss/**/*.scss")// Identificar el archivo .SCSS a compilar
+        .pipe( sourcemaps.init())
         .pipe( plumber())//Para si hay un error que no se finalice la ejecución
         .pipe( sass() )//Compilarlo
+        .pipe( postcss([autoprefixer(), cssnano()]))//Comprimir el codigo
+        .pipe( sourcemaps.write("."))
         .pipe( dest("build/css")) //Almacenarla en el disco duro
+    done();
+}
+
+function javascript(done) {
+    src("src/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write("."))
+        .pipe(dest("build/js"));
+    
     done();
 }
 
 function dev(done) {
     watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
+
     done();
 }
+
+
 
 function versionWebp(done) {
 
@@ -58,8 +82,11 @@ function imagenes(done) {
     done();
 }
 
+
+
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(versionWebp, versionAvif, imagenes, dev);
+exports.dev = parallel(versionWebp, versionAvif, imagenes, javascript, dev);
---
+++ FestivalMusica_inicio/src/js/app.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    crearGaleria();
+    // srollNav();
+    navegacionFija();
+
+}
+
+function navegacionFija() {
+    const barra = document.querySelector(".header");
+    const sobreFestival = document.querySelector(".sobre-festival");
+    const body = document.querySelector("body");
+
+    window.addEventListener("scroll", function() {
+        if(sobreFestival.getBoundingClientRect().bottom < 0) {
+            barra.classList.add("fijo");
+            body.classList.add("body-scroll");
+        } else {
+            barra.classList.remove("fijo");
+            body.classList.remove("body-scroll");
+
+        }
+        
+    });
+}
+
+// function srollNav() {
+//     // const enlaces = document.querySelectorAll(".navegacion-principal a")
+
+//     // enlaces.forEach( enlace => {
+//     //     enlace.addEventListener("click", function(e){
+//     //         e.preventDefault();
+
+//     //         const seccionScroll = e.target.attributes.href.value;
+//     //         const seccion = document.querySelector(seccionScroll);
+//     //         seccion.srollIntoView({behavior: "smooth"});
+//     //     });
+//     // })
+// }
+
+function crearGaleria() {
+    const galeria = document.querySelector(".galeria-imagenes");
+
+    for(let i =1; i <= 12; i++) {
+        const imagen = document.createElement("picture");
+        imagen.innerHTML = `
+            <source srcset="build/img/thumb/${i}.avif" type="picture/avif">
+            <source srcset="build/img/thumb/${i}.webp" type="picture/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen Galeria">
+        `;
+
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
+
+        galeria.appendChild(imagen);
+    }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement("picture");
+        imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="picture/avif">
+            <source srcset="build/img/grande/${id}.webp" type="picture/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen Galeria">
+        `;
+    
+    // crea overlay con la imagen
+    const overlay = document.createElement("div");
+    overlay.appendChild(imagen);
+    overlay.classList.add("overlay");
+
+    // Al hacer click en cualquier parte del overlay se quita la imagen
+    overlay.onclick = function() {
+        overlay.remove();
+        body.classList.remove("fijar-body")
+    }
+
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement("p");
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+    cerrarModal.onclick = function() {
+        overlay.remove();
+        body.classList.remove("fijar-body")
+    }
+    overlay.appendChild(cerrarModal);
+
+    // Añadirlo al HTML
+    const body = document.querySelector("body");
+    body.appendChild(overlay);
+    body.classList.add("fijar-body")
+}
