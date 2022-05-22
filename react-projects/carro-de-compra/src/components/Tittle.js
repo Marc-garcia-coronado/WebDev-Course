7adecb676e77c6847ca5c0f294ea3b681e4b8428
Marc Garcia <marcgaco@gmail.com>
2022-05-22 20:41:03 +0200

    adding react & much more

---
+++ react-projects/carro-de-compra/src/components/Tittle.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+
+const styles = {
+    titulo: {
+        marginBottom: "30px",    
+    }
+}
+
+class Tittle extends Component {
+    render () {
+        return(
+            <h1 style={styles.titulo}>Tienda</h1>
+        )
+    }
+}
+
+export default Tittle
---
+++ react-projects/mi-app/src/App.js
@@ -0,0 +1,33 @@
+import { Component } from "react";
+
+
+
+class Input extends Component {
+  state = { valor: ""}
+
+  handleChange = (value) => {
+    this.setState({valor: value})
+  }
+  render() {
+    return(
+      <input 
+        value={this.state.value}
+        onChange={e => this.handleChange(e.target.value)}
+      />
+    );
+  }
+}
+
+class App extends Component {
+
+  render() {
+    return (
+      <p>
+        Nombre completo:
+        <Input />
+        <Input />
+      </p>
+    );
+  }
+}
+export default App;
